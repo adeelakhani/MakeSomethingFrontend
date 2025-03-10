d7aaef9c97f059459888c04de3dd922f52326e65
Adeel Akhani <akhaniadeel@gmail.com>
2025-03-10 14:55:47 -0400

    adding analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from '../components/Header'
 import Body from '../components/Body.jsx'
-
+import { Analytics } from "@vercel/analytics/react"
 export default function App() {
   return (
     <>
       <Header /> 
       <Body />
+      <Analytics/>
     </>
   )
 }
