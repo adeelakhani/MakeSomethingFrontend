be9a124b29a2c2bd019a255ccdb21aae14100482
Adeel Akhani <akhaniadeel@gmail.com>
2025-03-10 15:00:07 -0400

    analytics retry

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Header from '../components/Header'
 import Body from '../components/Body.jsx'
-import { Analytics } from "@vercel/analytics/react"
 export default function App() {
   return (
     <>
       <Header /> 
       <Body />
-      <Analytics/>
     </>
   )
 }
